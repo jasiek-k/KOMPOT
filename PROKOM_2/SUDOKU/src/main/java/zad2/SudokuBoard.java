0a06ebe0e5e875090cc3e76d3e1972aacd2b5cc4
GH05Tx <43876190+GH05Tx@users.noreply.github.com>
2019-03-14 20:01:01 +0100

    task 2

---
+++ PROKOM_2/SUDOKU/src/main/java/zad2/SudokuBoard.java
@@ -0,0 +1,64 @@
+package zad2;
+
+import java.util.Random;
+
+public class SudokuBoard {
+    private int board[][] = new int[9][9];
+    private static final int UNASSIGNED = 0;
+
+    public SudokuBoard() {
+        board = new int[9][9];
+    }
+
+    public SudokuBoard(int sudoku[][]) {
+        this.board = sudoku;
+    }
+
+    private boolean containsInRow(int row, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean containsInCol(int col, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean containsInBox(int row, int col, int number) {
+        int r = row - row % 3;
+        int c = col - col % 3;
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isAllowed(int row, int col, int number) {
+        return !(containsInRow(row, number) || containsInCol(col, number) || containsInBox(row, col, number));
+    }
+
+
+
+
+
+    public void printBoard(int board[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
