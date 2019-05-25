faf5fead507f9a4bcaf0ece7ef0d8d0d56d17bfa
precel120 <darkuso@o2.pl>
2019-05-25 18:40:58 +0200

    zad 10 ciag dalszy

---
+++ Kompot1/View/src/main/java/PlanszaController.java
@@ -0,0 +1,38 @@
+import dao.FileSudokuBoardDao;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import zad2.SudokuBoard;
+
+public class PlanszaController {
+    private SudokuBoard sudokuBoard;
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private void initialize(){
+        sudokuBoard = MenuController.getSudokuBoard();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                TextField textField = new TextField();
+                textField.setText(Integer.toString(sudokuBoard.get(i,j)));
+                if(textField.getText().equals("0")){
+                    textField.clear();
+                    textField.setText(" ");
+                }
+                this.gridPane.add(textField,i,j);
+            }
+        }
+    }
+    @FXML
+    public void zapisz(){
+        FileSudokuBoardDao fileSudokuBoardDao = new FileSudokuBoardDao("gra.bin");
+        fileSudokuBoardDao.write(sudokuBoard);
+    }
+
+    @FXML
+    public void sprawdz(){
+        System.out.print("sprawdzone");
+    }
+}
