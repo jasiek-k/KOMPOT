ebc769773973c6b630701e7b4a58e2a26e43f3db
216797 Klamka Jan <216797@edu.p.lodz.pl>
2019-05-27 16:23:56 +0200

    task 10 fixed:

--- Kompot1/View/src/main/java/resources/LRB.java
+++ Kompot1/View/src/main/java/resources/LRB.java
@@ -2,13 +2,13 @@ package resources;
 
 import java.util.ListResourceBundle;
 
-public class Resources_pl extends ListResourceBundle {
+public class LRB extends ListResourceBundle {
 
     @Override
     protected Object[][] getContents() {
         return new Object[][] {
-                {"tytul", "Autorzy"},
-                {"autorzy", "Jan Klamka & Maciej Pracucik"}
+                {"title", "Authors"},
+                {"authors", "Jan Klamka & Maciej Pracucik"}
         };
     }
 }
